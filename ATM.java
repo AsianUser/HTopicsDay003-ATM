edd979fd0284c97f691fa4b924268b396bd478e2
BenR <90284651+AsianUser@users.noreply.github.com>
2023-08-30 12:54:20 -0700

    checkBalance & closeAccount Methods
    checkBalance
    checks if account exists
    if does, return balance
    
    closeAccount
    checks balance
    throws error if does not exist
    throw error if balance > 0
    else deletes key from accounts

--- ATM.java
+++ ATM.java
@@ -15,10 +15,18 @@ public class ATM {
         return Integer.toString(str.hashCode());
     }
 
-    // creates a new account from a UserId & amount
-    public void openAccount(String input, double amount) throws Exception {
+    public double checkBalance(String userID) throws Exception {
+        if (!accounts.containsKey(userID)) {
+            throw new Exception("Account does not exist");
+        }
+
+        return accounts.get(userID);
+    }
+
+    // creates a new account from a email & amount
+    public void openAccount(String email, double amount) throws Exception {
         // hashes userID
-        String userID = hashString(input);
+        String userID = hashString(email);
 
         // if there already exists and account, throw error
         if (accounts.containsKey(userID)) {
@@ -28,6 +36,20 @@ public class ATM {
         // else creates an account
         accounts.put(userID, amount);
 
+    }
+
+    public void closeAccount(String userID) throws Exception {
+        // get value given userID
+        // will throw exception if no exist
+        double value = checkBalance(userID);
+
+        // if has balance
+        if (value > 0) {
+            throw new Exception("Balance > 0\nPls withdraw all before try again");
+        }
+
+        // delet
+        accounts.remove(userID);
     }
 
 }
